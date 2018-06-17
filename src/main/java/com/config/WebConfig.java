401412677483da90b8664290f2688b9f1e327af4
Sachin Kumar <sjun1992@gmail.com>
2018-06-17 23:38:28 +0530

    In Skeleton App : Added login & registration controller

---
+++ src/main/java/com/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "GET", "DELETE", "POST", "OPTIONS")
+				.allowCredentials(true).maxAge(3600);
+	}
+
+}
